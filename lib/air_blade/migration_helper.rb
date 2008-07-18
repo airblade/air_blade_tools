2b00fa49fe1b2848ca6a33dd11cf2c1510597e2f
Andy Stewart <boss@airbladesoftware.com>
2008-07-18 08:31:35 +0000

    Improved method names.

--- lib/air_blade/migration_helper.rb
+++ lib/air_blade/migration_helper.rb
@@ -2,14 +2,14 @@
 module AirBlade
   module MigrationHelper
 
-    def fk(from_table, from_column, to_table)
+    def add_foreign_key(from_table, from_column, to_table)
       execute %(alter table #{from_table}
                 add constraint #{constraint_name(from_table, from_column)}
                 foreign key (#{from_column})
                 references #{to_table}(id))
     end
 
-    def drop_fk(from_table, from_column)
+    def drop_foreign_key(from_table, from_column)
       execute %(alter table #{from_table}
                 drop foreign key #{constraint_name(from_table, from_column)})
     end
