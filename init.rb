883292ba71ebbf9031d5e76370d6d8ee92d8ddc9
Andy Stewart <boss@airbladesoftware.com>
2011-04-01 13:17:03 +0100

    Omitted from the last commit.  Oops.

--- init.rb
+++ init.rb
@@ -1,9 +1,3 @@
-# http://drawohara.com/post/23559471  (modified a little by me).
-# See documentation for +pan_helper+ method.
-ActionController::Base.send :include, AirBlade::HelperHelper
-ActionController::Base.send :include, AirBlade::LayoutHelper
-ActionController::Base.send :protected, :pan_helper
-
 # Foreign key constraints.
 ActiveRecord::Migration.send :extend, AirBlade::Migrations::SchemaStatements
 ActiveRecord::ConnectionAdapters::TableDefinition.send :include, AirBlade::Migrations::SchemaDefinitions
